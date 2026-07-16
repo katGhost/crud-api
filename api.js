b064d77d486a9657bdb1dd613825e96f3f051e82
katGhost <katghost124@gmail.com>
2026-07-16 12:25:05 +0200

    feat: swaggerJSDoc configured, api-docs route success

--- api.js
+++ api.js
@@ -1,13 +1,35 @@
 import express from "express";
+import swaggerUI from "swagger-ui-express";
+import swaggerJSDoc from "swagger-jsdoc";
 import todoRoutes from "./routes/todos.js";
 
+
+// swagger options object
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: "CRUD API",
+      version: "1.0.0",
+      description: "A simple CRUD API"
+    },
+    servers: [
+      {
+        url: "http://localhost:3000"
+      },
+    ],
+  },
+  apis: ['./routes/*.js'],
+  
+};
+
+const specs = swaggerJSDoc(options);
+
 export const app = express();
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 
 // middleware
 app.use(express.json());
 app.use("/todos", todoRoutes);
 
-// test route
-/*app.get("/", (req, res, next) => {
-  res.json({"message": "Hello, World! I am back at Node.js"});
-})*/
